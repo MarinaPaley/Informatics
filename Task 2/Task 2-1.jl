b603e17b22f1aef764c807d7cb59650f8d1aa403
MarinaPaley <marina_paley@mail.ru>
2021-04-27 21:31:19 +0300

    Ввели enum #10
    При попытке ввода неправильного типа, выключаем Julia

--- Task 2/Task 2-1.jl
+++ Task 2/Task 2-1.jl
@@ -3,34 +3,43 @@ include("read_int.jl")
 include("get_triangle_area.jl");
 include("get_rectangle_area.jl");
 include("is_triangle_exists.jl");
-include("is_rectangle_exists.jl")
+include("is_rectangle_exists.jl");
 
 println("Task 2-1. Variant 16");
-figure = read_int("Choose figure for area calculation: reactangle = 0, triangle = 1");
-    #Get rectabgle area
-if (figure == 0)
-    length = read_double("Input length value = ");
-    width = read_double("Input width value = ");
+# Define enum type
+@enum Shape rectangle = 0  triangle = 1
 
-    if (is_rectangle_exists(length, width))
-        area = get_rectangle_area(length, width);
-        println("Rectangle area is $area");
+    user_input = read_int("Choose figure for area calculation: rectangle = 0, triangle = 1");
+    try
+        figure = Shape(user_input);
+    catch
+        println("You entered wrong value");
+        exit(1)
+    end;
+        #Get rectabgle area
+    if (figure == rectangle::Shape)
+        length = read_double("Input length value = ");
+        width = read_double("Input width value = ");
+
+        if (is_rectangle_exists(length, width))
+            area = get_rectangle_area(length, width);
+            println("Rectangle area is $area");
+        else
+            println("Error in input data");
+        end;
+
+        #Get triangle area
+    elseif (figure == triangle::Shape)
+        a = read_double("Input first side = ");
+        b = read_double("Input second side = ");
+        c = read_double("Input third side = ");
+
+        if is_triangle_exists(a, b, c)
+            area = get_triangle_area(a, b, c);
+            println("Triangle area is $area");
+        else
+            println("Error in input data");
+        end;
     else
         println("Error in input data");
     end;
-
-    #Get triangle area
-elseif (figure == 1)
-    a = read_double("Input first side = ");
-    b = read_double("Input second side = ");
-    c = read_double("Input third side = ");
-
-    if is_triangle_exists(a, b, c)
-        area = get_triangle_area(a, b, c);
-        println("Triangle area is $area");
-    else
-        println("Error in input data");
-    end;
-else
-    println("Error in input data");
-end;
